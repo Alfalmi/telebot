43c0bf5c7f431608b876343d59fe6d9c8fedb20b
pinooxd <pinooxd@gmail.com>
2021-04-22 02:50:52 -0700

    sada

--- telebot/credential.py
+++ telebot/credential.py
@@ -1,3 +1,3 @@
-bot_token = "1744434934:AAFBqF2SVX5AY37ePZUYoiIhG2eVod13u1k"
+bot_token = "1744434934:AAHNTOfkL0PBoDLA3dcfwk9mAasjYP0WNZQ"
 bot_user_name = "tontibot"
-URL = "https://git.heroku.com/sadtelebot.git"
+URL = "https://sadtelebot.herokuapp.com/"
