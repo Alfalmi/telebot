9028496f129bfe92f5a437a6a0c37305cfbcb447
pinooxd <pinooxd@gmail.com>
2021-04-22 04:51:13 -0700

    dons

--- telebot/credential.py
+++ telebot/credential.py
@@ -1,3 +1,3 @@
-bot_token = "1744434934:AAHNTOfkL0PBoDLA3dcfwk9mAasjYP0WNZQ"
+bot_token = "1744434934:AAHO_NI2UDiyjqL9P8a1o_AEXbxBnvoXlwo"
 bot_user_name = "tontibot"
-URL = "https://sadtelebot.herokuapp.com/"
+URL = "https://blownfuse.herokuapp.com/"
