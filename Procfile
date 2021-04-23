2e80c97201c5a90c0df9452b9ebe9c579e530721
pinooxd <pinooxd@gmail.com>
2021-04-22 19:11:15 -0700

    lol

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: gunicorn app:app
-worker: python app.py 
+web: python3 bot.py
--- telebot/credential.py
+++ telebot/credential.py
@@ -1,3 +1,3 @@
 bot_token = "1744434934:AAHO_NI2UDiyjqL9P8a1o_AEXbxBnvoXlwo"
 bot_user_name = "tontibot"
-URL = "https://blownfuse.herokuapp.com/"
+URL = "https://blewfuse.herokuapp.com//"
